0959c7b8e70d792db24191305a49f7a5ebaa431f
nacter242 <nhatanh0945062006@gmail.com>
2026-04-06 16:23:14 +0700

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,30 +1,32 @@
 @echo off
 cd /d "%~dp0"
-set PATH=C:\Program Files\CodeBlocks\MinGW\bin;%PATH%
-echo === BUILDING WORDSCAPES ===
+set PATH=C:\msys64\ucrt64\bin;%PATH%
+
+echo === DANG BUILD GAME WORDSCAPES ===
 
 g++ -g -std=c++17 ^
-  src\main.cpp ^
-  core\Game.cpp ^
-  core\Input.cpp ^
-  core\TextureManager.cpp ^
-  puzzle\Grid.cpp ^
-  puzzle\LetterWheel.cpp ^
-  puzzle\Level.cpp ^
-  puzzle\WordValidator.cpp ^
-  ui\Button.cpp ^
-  ui\Menu.cpp ^
-  ui\HUD.cpp ^
-  data\LevelLoader.cpp ^
+  src/main.cpp ^
+  core/Game.cpp ^
+  core/Input.cpp ^
+  core/TextureManager.cpp ^
+  puzzle/Grid.cpp ^
+  puzzle/LetterWheel.cpp ^
+  puzzle/Level.cpp ^
+  puzzle/WordValidator.cpp ^
+  ui/Button.cpp ^
+  ui/Menu.cpp ^
+  ui/HUD.cpp ^
+  data/LevelLoader.cpp ^
   -o game.exe ^
-  -IC:\SDL_LIBRARIES\include ^
-  -LC:\SDL_LIBRARIES\lib ^
-  -lSDL3 -lSDL3_ttf
+  -I"C:/msys64/ucrt64/include" ^
+  -L"C:/msys64/ucrt64/lib" ^
+  -lSDL3_image -lSDL3_ttf -lSDL3
 
 if %ERRORLEVEL% EQU 0 (
+    echo ---------------------------------------
     echo === BUILD THANH CONG! ===
-    echo Chay game.exe de choi...
+    .\game.exe
 ) else (
-    echo === BUILD LOI! ===
+    echo === BUILD LOI ===
 )
 pause
